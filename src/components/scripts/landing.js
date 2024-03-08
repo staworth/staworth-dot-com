4cc3505b844b8f1d565c7e8d66971ea5c27dc997
jackgale.eth <jack.gale@icloud.com>
2024-03-08 23:13:52 +0000

    Add delay to landing image

--- src/components/scripts/landing.js
+++ src/components/scripts/landing.js
@@ -1,6 +1,18 @@
 async function comingSoon() {
+    document.getElementById("landing-image").style.display = "None";
     document.getElementById("landing-text").style.display = "None";
-    setTimeout(showLandingText, 3000)
+    setTimeout(
+        showLandingImage,
+        1000
+    )
+    setTimeout(
+        showLandingText,
+        3000
+    )
+}
+
+function showLandingImage() {
+    document.getElementById("landing-image").style.display = "block";
 }
 
 function showLandingText() {
