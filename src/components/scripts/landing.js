2ca0fdc32b6cab93cd019b5d60affc5387c9b0af
jackgale.eth <jack.gale@icloud.com>
2024-03-11 07:54:36 +0000

    Update landing load animation

--- src/components/scripts/landing.js
+++ src/components/scripts/landing.js
@@ -1,20 +1,19 @@
 async function comingSoon() {
-    document.getElementById("landing-image").style.display = "None";
-    document.getElementById("landing-text").style.display = "None";
-    setTimeout(
-        showLandingImage,
-        1000
-    )
-    setTimeout(
-        showLandingText,
-        3000
-    )
+    await setTimeout(() => {
+        setIdDisplay("landing-image", "block");
+    }, 1000);
+    await setTimeout(() => {
+        setIdDisplay("landing-text", "block");
+    }, 5000);
+    await setTimeout(() => {
+        setIdDisplay("landing-image", "none");
+    }, 10000);
+    await setTimeout(() => {
+        setIdDisplay("landing-text", "none");
+    }, 10000);
 }
 
-function showLandingImage() {
-    document.getElementById("landing-image").style.display = "block";
-}
-
-function showLandingText() {
-    document.getElementById("landing-text").style.display = "block";
+function setIdDisplay(Id, display) {
+    document.getElementById(Id.toString()).style.display = display.toString();
+    console.log(Id + " is set as: " + document.getElementById(Id.toString()).style.display);
 }
